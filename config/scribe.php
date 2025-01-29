69ba508b38edcd24238bcdbb0e06b9db8de725ca
Said Abdulsalam <sabdulsalam@ubtconsults.se>
2025-01-29 12:30:14 +0100

    fix

--- config/scribe.php
+++ config/scribe.php
@@ -47,7 +47,7 @@ return [
     'type' => 'static',
 
     // See https://scribe.knuckles.wtf/laravel/reference/config#theme for supported options
-    'theme' => 'nasty',
+    'theme' => 'default',
 
     'static' => [
         // HTML documentation, assets and Postman collection will be generated to this folder.
